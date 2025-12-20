9323ee2c1f02759ebe3b9c9c9fed13013a70e436
Clément Lefèvre <lefevre.clementc@yahoo.com>
2025-12-20 11:57:42 +0100

    Connexion PDO fonctionnelle

--- main.php
+++ main.php
@@ -2,20 +2,20 @@
 
 require "bdd.php";
 
-// 1️⃣ On crée la connexion UNE FOIS
+
 $db = new DBConnect();
 $pdo = $db->getPDO();
 
-// (optionnel mais demandé par tes consignes)
+
 var_dump($pdo);
 
-// 2️⃣ Boucle CLI
+
 while (true) {
     $line = readline("Entrez votre commande : ");
 
     if ($line === "list") {
 
-        // ⬇️ TON CODE VA ICI ⬇️
+       
         $stmt = $pdo->query("SELECT * FROM contact");
 
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
